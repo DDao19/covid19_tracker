64fab452978d389e3c1271c6414cb686cf41a76c
Dung Dao <dung.dao19@outlook.com>
2020-07-23 20:14:26 -0500

    Updates

---
+++ precache-manifest.b7e87d7eb91df51c7b6a9accb0f3c579.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26464b4a0195f670a8d5d8016d62b21",
+    "url": "/covid19_tracker/index.html"
+  },
+  {
+    "revision": "113dd4094e106f62085d",
+    "url": "/covid19_tracker/static/css/main.05be8905.chunk.css"
+  },
+  {
+    "revision": "03cd361568861aaf7d8b",
+    "url": "/covid19_tracker/static/js/2.64ab80fd.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19_tracker/static/js/2.64ab80fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "113dd4094e106f62085d",
+    "url": "/covid19_tracker/static/js/main.8469f90b.chunk.js"
+  },
+  {
+    "revision": "8447e5eac2318c5f0b43",
+    "url": "/covid19_tracker/static/js/runtime-main.af0f8073.js"
+  },
+  {
+    "revision": "68ab962da8886ada6e756ae7de912758",
+    "url": "/covid19_tracker/static/media/img.68ab962d.jpg"
+  }
+]);
