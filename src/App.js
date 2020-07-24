5fa8c5ad0c90e18c75bf8b09cc22a694e2c5417e
Dung Dao <dung.dao19@outlook.com>
2020-07-23 20:11:53 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Cards, Chart, CountryPicker } from './components'
+import styles from './App.module.css'
+import { fetchData } from './api/index'
+import coronaImage from './images/img.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: ''
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    
+    this.setState({ data: fetchedData })
+  }
+
+  // method for selecting a country and grabbing the data for it
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country })
+  }
+
+  render() {
+    const { data, country } = this.state
+
+    return (
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <h1>COVID-19 TRACKER</h1>
+          <img className={styles.image} src={coronaImage} alt="COVID-19" />
+        </div>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/CountryPicker/CountryPicker.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react'
+import { NativeSelect, FormControl } from '@material-ui/core'
+import { fetchCountries } from '../../api/index'
+
+import styles from './CountryPicker.module.css'
+
+const CountryPicker = ({ handleCountryChange }) => {
+  const [fetchedCountries, setFetchedCountries] = useState([])
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setFetchedCountries(await fetchCountries())
+    }
+
+    fetchAPI()
+  }, [setFetchedCountries])
+
+  // mapping over the countries array to get a list of all the countries
+  const globalCountries = fetchedCountries.map((country, i) => {
+    return <option key={i} vallue={country}>{country}</option>
+  }) 
+
+  return (
+    <FormControl className={styles.formControl}>
+      <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
+        <option value="">Global</option>
+        {globalCountries}
+      </NativeSelect>
+    </FormControl>
+  )
+}
+
+
+export default CountryPicker
